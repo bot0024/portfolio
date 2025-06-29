3e40d5cbeecc6417f5fbe68d0001a360f566a0dd
dip nalawade <bothacks09@gmail.com>
2025-06-29 23:28:06 +0530

    Updates

---
+++ precache-manifest.17b121eaad6563431d18f3a586c84830.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16da8a95ec49db818876413d07806b57",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d59f8ff48d883a9e638b",
+    "url": "/portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "4c069c846443b229d134",
+    "url": "/portfolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "195b2815994acd2ea3d8",
+    "url": "/portfolio/static/css/main.8aca37e7.chunk.css"
+  },
+  {
+    "revision": "83ea5b528e8ac01f4cd5",
+    "url": "/portfolio/static/js/2.f510064c.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/portfolio/static/js/2.f510064c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d59f8ff48d883a9e638b",
+    "url": "/portfolio/static/js/3.e3311708.chunk.js"
+  },
+  {
+    "revision": "4c069c846443b229d134",
+    "url": "/portfolio/static/js/4.252496d9.chunk.js"
+  },
+  {
+    "revision": "195b2815994acd2ea3d8",
+    "url": "/portfolio/static/js/main.170cba9b.chunk.js"
+  },
+  {
+    "revision": "b60d29bb72716a8c227d",
+    "url": "/portfolio/static/js/runtime-main.727a145f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "577942d87698fe47c7a9ada0ce7adfcb",
+    "url": "/portfolio/static/media/Tata_logo.577942d8.svg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "40e31e385f7392c14029bb08d64d16f6",
+    "url": "/portfolio/static/media/elavatelabs.40e31e38.jpeg"
+  },
+  {
+    "revision": "0c0639f8bc6db4ae3ac59aa45b79b582",
+    "url": "/portfolio/static/media/google.0c0639f8.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "096cc86718fb367daf04b4786ba36753",
+    "url": "/portfolio/static/media/mmcc.096cc867.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "13f12b8d4d11a9e6fbe2157747bfa126",
+    "url": "/portfolio/static/media/resume.13f12b8d.pdf"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "f1a62bda44acdb20e57c2edaa6d62df6",
+    "url": "/portfolio/static/media/sppu.f1a62bda.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
